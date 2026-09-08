083e101f2158ac103ad3553b2297115c3ddd2707
phoo3011 <phooriwat_s@cmu.ac.th>
2026-09-09 01:49:25 +0700

    chore(api): allow localhost:3000 in CORS origin for local development

--- aiworkforce-backend/app.js
+++ aiworkforce-backend/app.js
@@ -5,6 +5,8 @@ function parseAllowedOrigins(value) {
   return (value || [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'https://aiworkforcedev.online',
